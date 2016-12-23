6360feed805f3323a48c822908522356265240c1
Peter Dinya <dinyapeti@gmail.com>
2016-12-23 13:58:44 +0100

    First commit. Base of look and feel is complete.

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  
+    $('.collapse').on('shown.bs.collapse', function(e) { 
+    		
+    });
+    $('.collapse').on('hidden.bs.collapse', function(e) { 
+    	
+    });
+   
+});
