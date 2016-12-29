9aff4d404ec4f38dd02230c422ac1fd0e21b58a4
Peter Dinya <dinyapeti@gmail.com>
2016-12-30 00:32:18 +0100

    RENAME: DefaultController to BlogController

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -14,6 +14,6 @@ class BlogController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('::base.html.twig');
+        return $this->render(':Blog:home.html.twig');
     }
 }
